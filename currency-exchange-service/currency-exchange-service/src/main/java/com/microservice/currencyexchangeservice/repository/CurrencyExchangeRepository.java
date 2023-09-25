243b50f3464440af777472a2172e375045ea09f7
Karthik J N <karthik.nagaraju@wisseninfotech.com>
2023-09-25 14:35:07 +0530

    first commit

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/repository/CurrencyExchangeRepository.java
@@ -0,0 +1,11 @@
+package com.microservice.currencyexchangeservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservice.currencyexchangeservice.entity.CurrencyExchange;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+
+	CurrencyExchange findByFromAndTo(String from, String to);
+
+}
